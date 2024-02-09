963fb3447f39e546802fddfcb141a607a9c7ab92
feroroko <feroroko@gmail.com>
2024-02-09 23:29:34 +0100

    oof

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ http.listen(3000, async function () {
     console.log("Server started at " + mainURL);
 
     try {
-        let client = await mongoClient.connect("mongodb+srv://feroroko:k5lhCGdv5VTLmj9B@cluster0.cvkmmi9.mongodb.net/");
+        let client = await mongoClient.connect("mongodb+srv://feroroko:3KR1Qp9bYxjF5tGN@cluster0.cvkmmi9.mongodb.net/");
         let database = client.db("FileShare");
         console.log("Database connected");
 
@@ -113,7 +113,7 @@ http.listen(3000, async function () {
                 request.status = "success";
                 request.message = "Signed up successfully. An email has been sent to verify your account. Once verified, you can log in and start using ShareFile.";
 
-                result.render("register", {
+                result.render("Register", {
                     "request": request
                 });
             } else {
@@ -128,4 +128,96 @@ http.listen(3000, async function () {
     } catch (error) {
         console.error("Error connecting to the database:", error);
     }
+
+    app.get("/verifyEmail/:email/:verification_token", async function (request, result) {
+        
+        let email = request.params.email;
+        let verification_token= request.params.verification_token;
+        
+        let user = await database.collection("users").findOne({
+            $and: [{ 
+             "email": email, 
+            },  {
+                "verification_token": parseInt(verification_token)
+            }]
+        });
+
+        if (user == null) {
+            request.status = "error";
+            request.message = "Email does not exists. Or verification link is epired.";
+            result.render("Login", {
+                "request": request
+            });
+        } else {
+            await database.collection("users").findOneAndUpdate( {
+                $and: [{ 
+                "email": email, 
+            }, { 
+               "verification_token": path.parseInt(
+                  verification_token)
+                }]
+            }, {
+                $set: { 
+                    "verification_token": "",
+                    "isVerified": true
+                }
+            });
+
+            request.status = "succes";
+            request.message = "Account has been verified. Please try login.";
+            result.render("Login", {
+                "request": request
+            });
+        }
+
+        app.get("/Login", function (request, result) {
+            result.render("Login", {
+                "request": request
+            }); 
+        });
+    
+        app.post("/Login", async function (request, result) {
+            let email = request.fields.email;
+            let password = request.fields.password;
+        
+            let user = await database.collection("users").findOne({
+                "email": email
+            });
+        
+            if (user == null) {
+                request.status = "error";
+                request.message = "Email does not exist.";
+                result.render("Login", {
+                    "request": request
+                });
+        
+                return false;
+            }
+        
+            bcrypt.compare(password, user.password, function (error, isVerify) {
+                if (isVerify) {
+                    if (user.isVerified) {
+                        request.session.user = user;
+                        result.redirect("/");
+        
+                        return false;
+                    }
+        
+                    request.status = "error";
+                    request.message = "Please verify your email.";
+                    result.render("Login", {
+                        "request": request
+                    });
+        
+                    return false;
+                }
+        
+                request.status = "error";
+                request.message = "Password is not correct";
+                result.render("Login", {
+                    "request": request
+                });
+            });
+        });
+    });
 });
