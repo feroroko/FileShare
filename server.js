18c47a7bc72af04bdb3f2cc058953420ae43d7aa
feroroko <feroroko@gmail.com>
2024-02-20 14:23:40 +0100

    14:23

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ let ObjectId = mongodb.ObjectId;
 const path = require('path');
 
 app.set("view engine", "ejs");
+const fileSystem = require("fs");
 
 app.use("/public/css", express.static(__dirname + "/public/css"));
 app.use("/public/js", express.static(__dirname + "/public/js"));
@@ -32,13 +33,14 @@ app.use(function (request, result, next) {
     next();
 });
 
+
 let formidable = require("express-formidable");
 app.use(formidable());
 
 let bcrypt = require("bcrypt");
 let nodemailer = require("nodemailer");
 
-let nodemailerFrom = "rokofero@gmail.com";
+let nodemailerFrom = "feroroko@gmail.com";
 let nodemailerObject = {
     service: "gmail",
     host: "smtp.gmail.com",
@@ -46,9 +48,51 @@ let nodemailerObject = {
     secure: true,
     auth: {
         user: "rokofero@gmail.com",
-        pass: ""
+        pass: "Mm00028444"
     }
 };
+
+// recursive function to get the forlder from uploaded
+function recursiveGetFolder(files, _id) {
+    let singleFile = null;
+
+    for (let a = 0; a < files.length; a++) {
+        const file = files[a];
+
+        // återkalla om fil typen är mapp och ID är hittad
+        if (file.type == "folder") {
+            if (file._id == _id) {
+                return file;
+            }
+
+            // Om den har filer, genomföra recursion
+            if (file.files.length > 0) {
+                singleFile = recursiveGetFolder(file.files, _id);
+                // Återkalla filen om den är hittad i sub-mapp
+                if (singleFile != null) {
+                    return singleFile;
+                }
+            }
+        }
+    }
+}
+
+// funcion för att lägga till en ny uppladdad objecjt och exportera den uppdaderade array:en
+function getUpdatedArray(arr, _id, uploadedOnj) {
+    for (let a = 0; a < arr.length; a++) {
+        //
+        if (arr[a].type == "folder") {
+            if (arr[a]._id == _id) {
+                arr[a]._id = ObjectId(arr[a]._id);
+            }
+            // om den innehåller filer genomför recursion
+            if (arr[a].files.length > 0) {
+                arr[a]._id = ObjectId(arr[a]._id);
+                getUpdatedArray(arr[a].files, _id, uploadedOnj);
+            }
+        }
+    }
+}
 
 http.listen(3000, async function () {
     console.log("Server started at " + mainURL);
@@ -57,6 +101,147 @@ http.listen(3000, async function () {
         let client = await mongoClient.connect("mongodb+srv://feroroko:3KR1Qp9bYxjF5tGN@cluster0.cvkmmi9.mongodb.net/");
         let database = client.db("FileShare");
         console.log("Database connected");
+
+        app.post("/CreateFolder", async function (request, result) {
+
+            const name = request.fields.name;
+            const _id = request.fields._id;
+
+            if (request.session.user) {
+
+                let user = await database.collection("users").findOne({
+                    "_id": ObjectId(request.session.user._id)
+                });
+
+                let uploadedOnj = {
+                    ":_id": ObjectId(),
+                    "type": "folder",
+                    "folderName": name,
+                    "files": [],
+                    "folderPath": "",
+                    "createdAt": new Date().getTime()
+                };
+
+                let folderPath = "";
+                let updatedArray = [];
+                if (_id == "") {
+                    folderPath = "public/uploads/" + user.email + "/" +
+                        name;
+                    uploadedOnj.folderPath = folderPath;
+
+                    if (!fileSystem.existsSync("public/uploads/" + user.
+                        email)) {
+                        fileSystem.mkdirSync("public/uploads/" + user.
+                            email);
+                    }
+                } else {
+                    let folderObj = await recursiveGetFolder(user.
+                        uploaded, _id);
+                    uploadedOnj.folderPath = folderObj.folderPath + "/"
+                        + name;
+                    updatedArray = await getUpdatedArray(user.uploaded,
+                        _id, uploadedOnj);
+                }
+
+                if (uploadedOnj.folderPath == "") {
+                    request.session.status = "error"
+                    request.session.message = "Folder name must not be emty.";
+                    result.redirect("/MyUploads");
+                    return false;
+                }
+
+                if (fileSystem.existsSync(uploadedOnj.folderPath)) {
+                    request.session.status = "error";
+                    request.session.message = "Folder with same name already exist";
+                    result.redirect("/MyUploads");
+                    return false;
+                }
+
+                fileSystem.mkdirSync(uploadedOnj.folderPath);
+
+                if (_id == "") {
+                    await database.collection("users").updateOne({
+                        "_id": ObjectId(request.session.user._id)
+                    }, {
+                        $push: {
+                            "uploaded": uploadedOnj
+                        }
+                    });
+                } else {
+
+                    for (let a = 0; a < updatedArray.length; a++) {
+                        updatedArray[a]._id = ObjectId(updatedArray[a].
+                            _id);
+                    }
+
+                    await database.collection("users").updateOne({
+                        "_id": ObjectId(request.session.user._id)
+                    }, {
+                        $set: {
+                            "uploaded": updatedArray
+                        }
+                    });
+
+                    result.redirect("/MyUploads" + _id);
+                    return false;
+                }
+
+                result.redirect("/Login");
+            }
+        });
+
+        app.get("/MyUploads/:_id?", async function (request, result) {
+            const _id = request.params._id;
+            if (request.session.user) {
+
+                let user = await database.collection("users").findOne({
+                    "_id": ObjectId(request.session.user._id)
+                });
+
+                let uploaded = null;
+                let folderName = "";
+                let createdAt = "";
+                if (typeof _id == "undefined") {
+                    uploaded = user.uploaded;
+                } else {
+                    let folderObj = await recursiveGetFolder(user.
+                        uploaded, _id);
+
+                    if (folderObj == null) {
+                        request.status = "error";
+                        request.message = "Folder not found.";
+                        result.render("MyUploads", {
+                            "request": request
+                        });
+                        return false;
+                    }
+
+                    uploaded = folderObj.files;
+                    folderName = folderObj.folderName;
+                    createdAt = folderObj.createdAt;
+                }
+
+                if (uploaded == null) {
+                    request.status = "error";
+                    request.message = "Directory not found.";
+                    result.render("MyUploads", {
+                        "request": request
+                    });
+                    return false;
+                }
+
+                result.render("MyUploads", {
+                    "request": request,
+                    "uploaded": uploaded,
+                    "_id": _id,
+                    "folderName": folderName,
+                    "createdAt": createdAt
+                });
+                return false;
+            }
+
+            result.redirect("/Login");
+        });
 
         app.get("/", function (request, result) {
             result.render("index", {
@@ -71,65 +256,82 @@ http.listen(3000, async function () {
         });
 
         app.post("/Register", async function (request, result) {
+            
             let name = request.fields.name;
             let email = request.fields.email;
             let password = request.fields.password;
             let reset_token = "";
             let isVerified = false;
             let verification_token = new Date().getTime();
-
+        
             let user = await database.collection("users").findOne({
                 "email": email
             });
 
-            if (!user) {
-                let hash = await bcrypt.hash(password, 10);
+            if (user == null) {
+                bcrypt.hash(password, 10, async function (error, hash){
+                    if (error) {
+                        console.error("Error hashing password:", error);
+                        // Handle the error, possibly by sending an error response to the user
+                        return;
+                    }
 
-                await database.collection("users").insertOne({
-                    "name": name,
-                    "email": email,
-                    "password": hash,
-                    "reset_token": reset_token,
-                    "uploaded": [],
-                    "sharedWithMe": [],
-                    "isVerified": isVerified,
-                    "verification_token": verification_token
-                });
+                    await database.collection("users").insertOne({
+                        "name": name,
+                        "email": email,
+                        "password": hash,
+                        "reset_token": reset_token,
+                        "uploaded": [],
+                        "sharedWithMe": [],
+                        "isVerified": isVerified,
+                        "verification_token": verification_token
+                    }, async function (error, data) {
 
-                let transporter = nodemailer.createTransport(nodemailerObject);
+                        let transporter = nodemailer.createTransport(
+                            nodemailerObject);
 
-                let text = "Please verify your account by clicking the following link: " +
-                    mainURL + "/verifyEmail/" + email + "/" + verification_token;
+                        let text = "Please verify your account by clicking the following link: " +
+                            mainURL + "/verifyEmail/" + email + "/" + verification_token;
 
-                let html = "Please verify your account by clicking the following link: <br><br> <a href='" + mainURL + "/verifyEmail/" + email + "/" + verification_token + "'>Confirm Email</a> < br><br> Thank you.";
+                        let html = "Please verify your account by clicking the following link: <br><br> <a href='" + mainURL + "/verifyEmail/" + email + "/" + verification_token + "'>Confirm Email</a><br><br> Thank you.";
 
-                await transporter.sendMail({
-                    from: nodemailerFrom,
-                    to: email,
-                    subject: "Email Verification",
-                    text: text,
-                    html: html
-                });
+                        await transporter.sendMail({
+                            from: nodemailerFrom,
+                            to: email,
+                            subject: "Email Verification",
+                            text: text,
+                            html: html
+                        }, function (error, info) {
+                            if (error) {
+                                console.error("Error sending email:", error);
+                            } else {
+                                console.log("Email sent: " + info.response);
+                            }    
+                            
+                            request.status = "success";
+                            request.message = "Signed up successfully. An email has been sent to verify your account. Once verified, you can log in and start using ShareFile.";
 
-                request.status = "success";
-                request.message = "Signed up successfully. An email has been sent to verify your account. Once verified, you can log in and start using ShareFile.";
-
-                result.render("Register", {
-                    "request": request
-                });
+                            result.render("Register", {
+                                "request": request
+                            });
+                        });
+                    });
+                });   
             } else {
                 request.status = "error";
-                request.message = "Email already exists.";
+                request.message ="Email already exist";
 
                 result.render("Register", {
                     "request": request
                 });
-            }
+            }   
+
         });
+
     } catch (error) {
         console.error("Error connecting to the database:", error);
     }
-
+      
     app.get("/verifyEmail/:email/:verification_token", async function (request, result) {
         let email = request.params.email;
         let verification_token = request.params.verification_token;
@@ -196,7 +398,7 @@ http.listen(3000, async function () {
         if (user == null) {
             request.status = "error";
             request.message = "Email does not exist.";
-            result.render("Login", {
+            return result.render("Login", {
                 "request": request,
             });
 
@@ -228,7 +430,7 @@ http.listen(3000, async function () {
             });
         });
 
-        app.post("ResetPassword", async function (request, result) {
+        app.post("/ResetPassword", async function (request, result) {
             let email = request.fields.email;
             let reset_token = request.fields.reset_token;
             let new_password = request.fields.new_password;
@@ -268,7 +470,7 @@ http.listen(3000, async function () {
             }
 
             bcrypt.hash(new_password, 10, async function (error, hash) {
-                await database.collection("users"), findOneAndUpdate({
+                await database.collection("users").findOneAndUpdate({
                     $and: [{
                         "email": email,
                     }, {
